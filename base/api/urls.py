49dc70855057b9c9382c50b9038b7bebb4458fcf
Rayyan Ahmed <rayyan1@mailbox.unideb.hu>
2026-03-18 04:22:21 +0100

    Change:Modify url routing

--- base/api/urls.py
+++ base/api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.getRoutes,name='routes'),
-    path('rooms/',views.getRooms,name='rooms'),
-    path('room/<int:pk>',views.getRoom,name='room')
+    path('rooms/',views.getRooms,name='apirooms'),
+    path('room/<int:pk>',views.getRoom,name='apiroom')
 ]
