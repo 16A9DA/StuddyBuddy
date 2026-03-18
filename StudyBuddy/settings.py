d9a4b15d01e727233909158280623c9c135b48eb
Rayyan Ahmed <rayyan1@mailbox.unideb.hu>
2026-03-18 04:25:20 +0100

    Addition of user

--- StudyBuddy/settings.py
+++ StudyBuddy/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'base',
+    'base.apps.BaseConfig',
     'rest_framework',
 ]
+
+
+AUTH_USER_MODEL = 'base.User'
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
