19d2c69248c1c76686e59a09bbc5758ec59ab808
Rayyan Ahmed <rayyan1@mailbox.unideb.hu>
2026-03-19 05:07:48 +0100

    Changes: made email / username fields 'imp'

--- base/models.py
+++ base/models.py
@@ -9,8 +9,8 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True)
     bio = models.TextField(null=True, blank=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
     # need to install pillow for this field to work
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, default="avatars/images.jpeg")
 
