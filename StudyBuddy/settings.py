ae568a6f99da0341e3f0e57c8053c287ce541006
Rayyan Ahmed <05ahmedrayyan@gmail.com>
2026-05-24 06:33:26 +0200

    Changes to deploy in render

--- StudyBuddy/settings.py
+++ StudyBuddy/settings.py
@@ -26,9 +26,10 @@ SECRET_KEY = os.getenv('SECRET_KEY','django-insecure-local-dev-only')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 # Application definition
